a9bde470aa05455e73f853304f21d9210fc3464f
Reda AGAOUA <reda.agaoua@gmail.com>
2024-12-23 23:40:48 +0100

    add profile.d

---
+++ profile.d/git.sh
@@ -0,0 +1,4 @@
+alias gs="git status"
+alias gb="git branch"
+alias gdc="git diff --color"
+alias gdcc="git diff --word-diff=color"
---
+++ profile.d/kubectl.sh
@@ -0,0 +1,4 @@
+alias k="kubectl"
+alias ka="kubectl apply -f"
+alias kd="kubectl describe"
+alias kdp="kubectl describe pod"
