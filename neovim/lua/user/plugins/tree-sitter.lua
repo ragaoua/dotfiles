4a8bc1818682398301c4327484e165dd1d709a43
Reda AGAOUA <reda.agaoua@gmail.com>
2025-07-18 10:57:59 +0200

    neovim : comment highlighting + tree-sitter

---
+++ neovim/lua/user/plugins/tree-sitter.lua
@@ -0,0 +1,25 @@
+return { -- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"go",
+			"python",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+		},
+		-- Autoinstall languages that are not installed
+		auto_install = true,
+		indent = { enable = true },
+	},
+}
