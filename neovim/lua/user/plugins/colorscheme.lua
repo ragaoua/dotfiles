1678a709f9788a50f06773f86bf28118a6b72309
Reda AGAOUA <reda.agaoua@gmail.com>
2025-07-20 00:24:33 +0200

    neovim : customize nord colorscheme

--- neovim/lua/user/plugins/colorscheme.lua
+++ neovim/lua/user/plugins/colorscheme.lua
@@ -2,6 +2,19 @@ return {
 	"shaunsingh/nord.nvim",
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("nord")
+		vim.g.nord_bold = false
+		vim.g.nord_italic = false
+		require("nord").set()
+
+		-- Change variables highlighting
+		vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+			group = vim.api.nvim_create_augroup("nord-lsp-highlight", {}),
+			pattern = "*",
+			callback = function()
+				vim.api.nvim_set_hl(0, "LspReferenceRead", { fg = "#FFFFFF", bg = "#357973" })
+				vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg = "#FFFFFF", bg = "#357973" })
+				vim.api.nvim_set_hl(0, "LspReferenceText", { fg = "#FFFFFF", bg = "#357973" })
+			end,
+		})
 	end,
 }
