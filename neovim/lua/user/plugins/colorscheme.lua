d484180a6a71a950f7f5254c42becb6b169db936
Reda AGAOUA <reda.agaoua@gmail.com>
2025-07-21 22:31:35 +0200

    neovim : switch to catppuccin theme

--- neovim/lua/user/plugins/colorscheme.lua
+++ neovim/lua/user/plugins/colorscheme.lua
@@ -1,20 +1,12 @@
 return {
-	"shaunsingh/nord.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.g.nord_bold = false
-		vim.g.nord_italic = false
-		require("nord").set()
-
-		-- Change variables highlighting
-		vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
-			group = vim.api.nvim_create_augroup("nord-lsp-highlight", {}),
-			pattern = "*",
-			callback = function()
-				vim.api.nvim_set_hl(0, "LspReferenceRead", { fg = "#FFFFFF", bg = "#357973" })
-				vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg = "#FFFFFF", bg = "#357973" })
-				vim.api.nvim_set_hl(0, "LspReferenceText", { fg = "#FFFFFF", bg = "#357973" })
-			end,
+		require("catppuccin").setup({
+			flavour = "macchiato",
+			show_end_of_buffer = true,
 		})
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
