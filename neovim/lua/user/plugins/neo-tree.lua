466d3cd4aeb95dc27ed836e2250b7980ee6cf964
Reda AGAOUA <reda.agaoua@gmail.com>
2025-07-22 22:33:40 +0200

    Remove keymap to dhow buffers in neo-tree explorer
    I'll use telescope to browse open buffers instead

--- neovim/lua/user/plugins/neo-tree.lua
+++ neovim/lua/user/plugins/neo-tree.lua
@@ -40,16 +40,6 @@ return {
 			end,
 			desc = "NeoTree toggle neovim filesystem",
 		},
-		{
-			"<leader>be",
-			function()
-				require("neo-tree.command").execute({
-					toggle = true,
-					source = "buffers",
-				})
-			end,
-			desc = "NeoTree toggle buffers",
-		},
 	},
 	opts = {
 		close_if_last_window = true,
