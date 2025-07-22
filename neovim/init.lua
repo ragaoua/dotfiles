b446b60b84e49cf2a1667aa0358868ccc868ceb2
Reda AGAOUA <reda.agaoua@gmail.com>
2025-07-22 22:27:35 +0200

    minor comment

--- neovim/init.lua
+++ neovim/init.lua
@@ -3,4 +3,8 @@ require("user.options")
 require("user.keymaps")
 require("user.autocommands")
 require("user.floating-terminal")
+
+-- NOTE : the above are default configs that do not rely on any plugins or access to a plugin manager.
+-- Thus, it is important to require the lazy config only at end, to allow it to override the default options
+-- and keymaps
 require("user.lazy")
