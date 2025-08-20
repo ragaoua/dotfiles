a64c57a340942c1ac2b861bc809c4b59b89e9651
Reda AGAOUA <reda.agaoua@gmail.com>
2025-08-21 00:46:42 +0200

    tmux : unbind default prefix

---
+++ scripts/.config/scripts/open_project_session.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# shellcheck disable=SC2155
+readonly search_dirs="$(fd . "${HOME}/Projects" --type=directory --max-depth=1)"
+readonly project_full_path="$(echo "${search_dirs[@]}" | fzf)"
+readonly project_name="$(basename "$project_full_path")"
+
+if ! tmux has-session -t "$project_name" 2>/dev/null ; then
+  tmux new-session -d -s "$project_name" -c "$project_full_path"
+fi
+
+cmd="attach-session"
+if [ ! -z "$TMUX" ] ; then
+  cmd="switch-client"
+fi
+
+tmux "$cmd" -t "$project_name"
