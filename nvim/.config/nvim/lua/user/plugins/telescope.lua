dc15b28a76a2aae36503270e91526271b9b9d7bd
Reda AGAOUA <reda.agaoua@gmail.com>
2025-08-23 22:41:36 +0200

    nvim : fix telescope oldfiles keymap

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -43,7 +43,7 @@ return {
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>so", builtin.find_files, { desc = "[S]earch [O]ld files" })
+		vim.keymap.set("n", "<leader>so", builtin.oldfiles, { desc = "[S]earch [O]ld files" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[ ] Find existing buffers" })
