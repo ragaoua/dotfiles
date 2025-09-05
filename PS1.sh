0b04b3c0b8dcc34dc030e773bc89cb469cde8919
Reda AGAOUA <reda.agaoua@gmail.com>
2025-09-06 00:07:41 +0200

    PS1 : display a flag when stash exists in cwd's git repo

--- PS1.sh
+++ PS1.sh
@@ -1,2 +1,2 @@
-PS1='[\[$(if [[ $EUID -eq 0 ]]; then echo "\e[91m"; else echo "\e[33m"; fi)\]\u\[\e[00m\]@\[\e[36m\]\w\[\e[m\]\[\e[32m\]$(git branch --show-current 2>/dev/null | sed "s/.*/ (&)/")\[\e[00m\]] $ '
+PS1='[\[$(if [[ $EUID -eq 0 ]]; then echo "\e[91m"; else echo "\e[33m"; fi)\]\u\[\e[00m\]@\[\e[36m\]\w\[\e[m\]\[\e[32m\]$(echo $(git branch --show-current 2>/dev/null)$(git stash list | grep -q . && echo " ⚑") | sed "s/.*/ (&)/")\[\e[00m\]] $ '
 
