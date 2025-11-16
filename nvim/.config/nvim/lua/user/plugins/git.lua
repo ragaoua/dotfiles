1d5c7f81aa55b8628fc54d0261aa457c7b3fe94a
Reda AGAOUA <reda.agaoua@gmail.com>
2025-11-16 22:23:15 +0100

    git : add <leader>g keymap to toggle fugitive

--- nvim/.config/nvim/lua/user/plugins/git.lua
+++ nvim/.config/nvim/lua/user/plugins/git.lua
@@ -13,5 +13,20 @@ return {
 	},
 	{
 		"tpope/vim-fugitive",
+		config = function()
+			local function fugitive_toggle()
+				for _, win in ipairs(vim.api.nvim_list_wins()) do
+					local buf = vim.api.nvim_win_get_buf(win)
+					local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+					if ft == "fugitive" then
+						vim.api.nvim_win_close(win, true)
+						return
+					end
+				end
+				vim.cmd("G")
+			end
+
+			vim.keymap.set("n", "<leader>g", fugitive_toggle, { desc = "Toggle Git Fugitive" })
+		end,
 	},
 }
