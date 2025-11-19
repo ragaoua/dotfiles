8f01097276985443f83c594fd77ff82578d8f881
Reda AGAOUA <reda.agaoua@gmail.com>
2025-11-19 23:08:17 +0100

    neovim : reduce scrolloff

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -3,8 +3,8 @@ vim.opt.number = true -- Line numbers
 vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.cursorline = true -- Highlight current line
 vim.opt.wrap = false -- Don't wrap lines
-vim.opt.scrolloff = 10 -- Keep n lines above/below cursor
-vim.opt.sidescrolloff = 20 -- Keep n columns left/right of cursor
+vim.opt.scrolloff = 5 -- Keep n lines above/below cursor
+vim.opt.sidescrolloff = 10 -- Keep n columns left/right of cursor
 
 -- Indentation
 vim.opt.tabstop = 2 -- Tab width
