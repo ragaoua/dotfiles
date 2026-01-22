eaa5d9cfce8ceef320177d1d276f1c21269eea6d
Reda AGAOUA <reda.agaoua@gmail.com>
2026-01-22 21:53:00 +0100

    tmux scripts: check tmux session

--- tmux/.config/tmux/kill_current_session.sh
+++ tmux/.config/tmux/kill_current_session.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
 # Kill the current tmux session after switching to the session that was last attached before it.
 
 # shellcheck disable=SC2155
+
+set -euo pipefail
+
+if [ -z "${TMUX:-}" ] ; then
+  echo >&2 "Not in a tmux session."
+  exit 1
+fi
+
 readonly current_session="$(tmux display-message -p "#S")"
 readonly previous_session="$(
   tmux list-sessions -F "#{session_last_attached} #{session_name}" |
