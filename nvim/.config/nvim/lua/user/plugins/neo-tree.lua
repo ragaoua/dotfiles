9d8aab7b0b3e6e467f09698d494351c8b9c994e2
Reda AGAOUA <reda.agaoua@gmail.fr>
2026-03-11 09:38:54 +0100

    Move neotree filetree to the left (default)

--- nvim/.config/nvim/lua/user/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/user/plugins/neo-tree.lua
@@ -36,10 +36,6 @@ return {
 		},
 	},
 	opts = {
-		-- close_if_last_window = true,
-		window = {
-			position = "float",
-		},
 		filesystem = {
 			-- hijack_netrw_behavior = "open_current",
 			filtered_items = {
