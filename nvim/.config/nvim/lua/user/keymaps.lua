4447b959225f44695f905652bd1bcdb0d9fc3084
Reda AGAOUA <reda.agaoua@gmail.fr>
2026-04-30 10:03:21 +0200

    nvim: remove usunsed resizing/splitting keymaps and adjust width resizing

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -33,12 +33,8 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
 -- Splitting & Resizing
-vim.keymap.set("n", "<C-s>v", ":vsplit<CR>", { desc = "Split window vertically" })
-vim.keymap.set("n", "<C-s>h", ":split<CR>", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+vim.keymap.set("n", "<C-w><", ":vertical resize -10<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-w>>", ":vertical resize +10<CR>", { desc = "Increase window width" })
 
 -- Move lines up/down
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
