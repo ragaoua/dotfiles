28eae5196a4b655cb8dd9d5d3c96b5b5fc39f705
Reda AGAOUA <reda.agaoua@gmail.fr>
2026-07-15 15:09:04 +0200

    nvim: open pdf's in system default viewer

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -2,28 +2,29 @@ local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup,
-  callback = function()
-    vim.hl.on_yank()
-  end,
+	group = augroup,
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 -- Return to last edit position when opening files
 vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
+	group = augroup,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
 
--- Open image files in the system default viewer instead of loading them as a buffer
+-- Open these file formats in the system default viewer instead of loading them as a buffer
 vim.api.nvim_create_autocmd("BufReadCmd", {
 	group = augroup,
 	pattern = {
+		"*.pdf",
 		"*.png",
 		"*.jpg",
 		"*.jpeg",
