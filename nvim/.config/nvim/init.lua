b7a9bdb0eed3275c565148db5622cf6a655b6e7b
Reda AGAOUA <reda.agaoua@gmail.fr>
2026-07-21 09:34:51 +0200

    nvim: delete floating-terminal
    I never use it, I just spawn a new tmux pane

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,6 @@ require("user.colorscheme")
 require("user.options")
 require("user.keymaps")
 require("user.autocommands")
-require("user.floating-terminal")
 
 -- NOTE : the above are default configs that do not rely on any plugins or access to a plugin manager.
 -- Thus, it is important to require the lazy config only at end, to allow it to override the default options
