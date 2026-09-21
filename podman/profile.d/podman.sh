435423d4a784f6fc59042a5148c0c99610c14ba9
Reda Agaoua <reda.agaoua@gmail.com>
2026-09-21 22:51:43 +0200

    podman: DOCKER_HOST now set only if podman machine is runnin

--- podman/profile.d/podman.sh
+++ podman/profile.d/podman.sh
@@ -1,3 +1,8 @@
 alias pc="podman-compose"
-export DOCKER_HOST="unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')"
-export TESTCONTAINERS_RYUK_DISABLED=true
+
+__podman_machine="$(podman machine inspect --format '{{.State}} {{.ConnectionInfo.PodmanSocket.Path}}' 2>/dev/null | cut -d' ' -f1)"
+if [ "$__podman_machine" == "running" ]; then
+  export DOCKER_HOST="unix://${__podman_machine#* }"
+  export TESTCONTAINERS_RYUK_DISABLED=true
+fi
+unset __podman_machine
